a9474f65a20ae1727136a1ac19dbdac26815c831
uktechreviews <spencerorgan1@me.com>
2016-09-27 14:05:21 +0100

    Delete write.py

--- write.py
+++
@@ -1,7 +0,0 @@
-calendar = open('config.advent','w')
-
-for days in range (1,10):
-        text = 'Day:' + str(days) + " open" + "\n"
-        print (text)
-        calendar.write(text)
-calendar.close()
