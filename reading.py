c460d97d82ae661d73cdb589ae3804441b7c626f
uktechreviews <spencerorgan1@me.com>
2016-09-27 14:05:28 +0100

    Delete reading.py

--- reading.py
+++
@@ -1,9 +0,0 @@
-calendar = open('config.advent','r')
-
-for days in range (1,25):
-	content = calendar.readline()
-	if "open" in content:
-		print ("Door open on day " + str(days))
-calendar.close()
-	
-
